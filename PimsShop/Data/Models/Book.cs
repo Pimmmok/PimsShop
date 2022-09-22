5d6a2d9511b66dc253903e5ae8dfeac0be5f4d8c
Вадим Качан <pim_reffery@mail.ru>
2022-09-22 22:25:30 +0300

    Add project files.

---
+++ PimsShop/Data/Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PimsShop.Data.Models
+{
+    public class Book
+    {
+        public int id { set; get; }
+        public string bookName { set; get; }
+        public string shortDescription { set; get; }
+        public string longDescription { set; get; }
+        public string img { set; get; }
+        public ushort price { set; get; }
+
+        public bool isFavorite { set; get; }
+        public int available { set; get; }
+
+        public int categoryId { set; get; }
+
+        public virtual Category category { set; get; }
+
+    }
+}
